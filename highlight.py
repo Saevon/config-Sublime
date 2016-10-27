08068bdf165417fca78721cb9c3ffc3ed29081d4
Serghei Filippov <blastowind@gmail.com>
2016-10-26 22:42:30 -0400

    Adds sublime debug handler

--- highlight.py
+++ highlight.py
@@ -234,8 +234,17 @@ class ClearAllHighlightsCommand(sublime_plugin.TextCommand):
 
 
 
+SUBLIME_DEBUG_STATUS = {}
 
 
+class DebugSublimeToggleCommandsCommand(sublime_plugin.ApplicationCommand):
+
+    def run(self):
+        status = not SUBLIME_DEBUG_STATUS.get('commands', False)
+        SUBLIME_DEBUG_STATUS['commands'] = status
+
+        sublime.log_commands(status)
+        sublime.log_input(status)
 
 
 
