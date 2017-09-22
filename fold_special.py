6a4cad80bdaa940dbec239fa61f4e55b15010c24
Serghei Filippov <serghei.filippov@gmail.com>
2017-09-22 13:30:27 -0700

    Adds & Improves JS Folding

--- fold_special.py
+++ fold_special.py
@@ -8,8 +8,29 @@ def sublime_show_region(view, region):
         view.show_at_center(region)
 
 
+def isWhitespace(str):
+    return len(str.lstrip()) == 0
+
+
+
 class FoldSpecialCommand(sublime_plugin.TextCommand):
-    FOLD_PATTERN = r"it\('([^']|\\')*', *(function)? *\((done)?\) *(=>)? *{"
+    FOLD_PATTERN = (
+        r"(it|itErrors)"
+        r"\( *"
+        r"("
+            r"([']([^']|\\['])*')"
+            r"|([`]([^`]|\\[`])*`)"
+            r'|(["]([^"]|\\["])*")'
+        r")"
+
+        # Optional Second Argument
+        r"("
+            r" *, *"
+            r"[^,]+"
+        r")?"
+
+        r" *, *(function)? *\((done)?\) *(=>)? *{\n?"
+    )
 
     def run(self, edit=None, **kwargs):
         selected = self.view.sel()
@@ -31,7 +52,29 @@ class FoldSpecialCommand(sublime_plugin.TextCommand):
             # Inbuild Fold Command, needs a start + end to fold a region
             # self.view.fold(matched_regions)
             # Not as good, as it needs the selection to exist (thus we need to revert the selection + view)
-            self.view.run_command('fold')
+            # self.view.run_command('fold')
+            # Also not great as this changes selection
+            self.view.run_command('expand_selection', args={'to': 'scope'})
+
+            # Normalize the region: ensures reverse regions are flipped
+            fold_region = self.view.sel()[0]
+            fold_region = sublime.Region(
+                fold_region.begin(),
+                fold_region.end(),
+            )
+
+            # Strip starting spaces from a region
+            while fold_region.b > fold_region.a and isWhitespace(self.view.substr(fold_region.a).lstrip()):
+                fold_region.a += 1
+
+
+            # Strip trailing spaces from a region
+            while fold_region.b > fold_region.a and isWhitespace(self.view.substr(fold_region.b - 1)):
+                fold_region.b -= 1
+
+
+            self.view.fold(fold_region)
+
 
         # Reset the cursor
         self.view.sel().clear()
@@ -40,5 +83,13 @@ class FoldSpecialCommand(sublime_plugin.TextCommand):
 
         if len(selection_backup):
             sublime_show_region(self.view, selection_backup[0])
+
+
+class FoldSetupCommand(FoldSpecialCommand):
+    FOLD_PATTERN = (
+        r"(before|after)(Each)?"
+        "\(.*{"
+    )
+
 
 
