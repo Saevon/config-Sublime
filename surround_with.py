ed90f456f8ee308380028004afd208d0e63efc74
Serghei Filippov <serghei.filippov@gmail.com>
2018-01-26 16:26:21 -0800

    Improves Surround with plugin
     * Multisurround support

--- surround_with.py
+++ surround_with.py
@@ -1,3 +1,4 @@
+import sublime
 import sublime_plugin
 
 
@@ -9,6 +10,9 @@ class SurroundWith(sublime_plugin.TextCommand):
     '''
 
     def run(self, edit, character, expand=False):
+        # This should already be sorted
+        orig_selection = [region for region in self.view.sel()]
+
         if character in "{}":
             start = "{"
             end = "}"
@@ -27,7 +31,9 @@ class SurroundWith(sublime_plugin.TextCommand):
             # Also allow weird characters
             start = end = character
         else:
-            print("Can't surround this character", character)
+            # TODO: status bar message?
+            print("Can't surround with character: '{}'".format(character))
+            return;
 
         if expand:
             self.view.run_command("enter_visual_mode")
@@ -36,6 +42,20 @@ class SurroundWith(sublime_plugin.TextCommand):
         for sel in self.view.sel():
             self.view.insert(edit, sel.begin(), start)
             self.view.insert(edit, sel.end() + 1, end)
+
+        # Reset each selection in order, adding the resulting offset for each change
+        offset = 0
+        self.view.sel().clear()
+        for selection in orig_selection:
+            offset += len(start)
+
+            self.view.sel().add(sublime.Region(
+                selection.begin() + offset,
+                selection.end() + offset,
+            ))
+
+            # Now add the "end" offset for the next selection
+            offset += len(end)
 
         # Save the transaction
         self.view.end_edit(edit)
